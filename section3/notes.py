c185b566fba6b22d7eb0fc2cbfa194ce1966ee84
arthurarabelo <arthurrabelo2015@hotmail.com>
2024-01-05 19:56:20 -0300

    sec3-functionprint

---
+++ section3/notes.py
@@ -0,0 +1,12 @@
+# comentários são feitos com '#'
+# "comentários" multilinhas são feitos com """ ... """ ou com ''' ... ''' -> são chamados de docstring
+
+'''
+    Função print:
+
+    -> Possui quebra de linha e separador espaço por padrão 
+    -> É possível mudar o separador dos argumentos:
+            print(1, 2, sep = '')
+    -> É possível adicionar final da impressão (substitui o '\n'):
+            print(1, 2, end = '')
+''' 

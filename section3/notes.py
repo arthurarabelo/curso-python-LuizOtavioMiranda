b2c1e90baf7256b68745664d21c974d085512bfe
arthurarabelo <arthurrabelo2015@hotmail.com>
2024-01-08 19:43:28 -0300

    3rd commit

--- section3/notes.py
+++ section3/notes.py
@@ -8,9 +8,7 @@
             print(1, 2, sep = '')
         -> É possível adicionar final da impressão (substitui o '\n'):
             print(1, 2, end = '')
-'''
-
-'''
+            
     Tipos de dados:
         -> Python tem tipagem dinâmica e forte, ou seja, já reconhece o tipo sem que seja preciso o declarar explicitamente.
         -> A função type mostra o tipo que o Python atribuiu à variável: type(1), type('Arthur'), type(1.5)
@@ -26,4 +24,53 @@
             nome_completo =  'Arthur Araujo Rabelo'
             idade = 20
             maior_de_idade = idade >= 18
+
+    Operadores aritméticos:
+        divisão inteira: //
+        exponenciação: **
+        -> O operador + concatena strings também.
+        -> O operador * também faz a repetição de uma string.
+        -> Precedência de operadores:
+            1. (x + y)
+            2. **
+            3. *, /, //, %
+            4. +, -
+
+    fStrings (formatação de string):
+        str = f'{variavel}...{outra variavel}'
+        -> Formatar casas decimais de um número: f'{variavel do numero:.xf}...', sendo x o número de casas.
+        -> É possível colocar vírgula para dividir o número: f'{variavel do numero:,.xf}...'.   
+
+    Format:
+        str.format() -> format é um método do objeto string
+
+    Função input:
+        Exemplo: 
+        nome = input('Qual é o seu nome?') -> nome é do tipo string
+
+    Operações condicionais:
+        if [expressao]:
+            ...
+        elif [expressao]:
+            ...
+        else:
+            ...
+        
+    Operadores lógicos:
+        && -> and
+        || -> or
+        ! -> not
+
+        Falsy: 0, 0.0, '', False
+
+    Operadores in e not in:
+        Exemplo:
+            nome = 'Arthur'
+            print('r' in nome) -> retorna True
+    
+    Interpolação básica de strings:
+        s - string
+        d e i - int
+        f - float
+        x e X - hexadecimal (ABCDEF0123456789)
 '''

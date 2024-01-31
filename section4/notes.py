6894afdd88e48afe9a55a4b869910d50a0708ed5
arthurarabelo <arthurrabelo2015@hotmail.com>
2024-01-31 19:46:23 -0300

    section4 started

---
+++ section4/notes.py
@@ -0,0 +1,14 @@
+"""
+Funções: retornam None por padrão
+    def functionName(parameters):
+        ...
+
+    functionName(args)
+
+Argumentos nomeados e não nomeados(posicional) em funções:
+    def soma(x, y):
+        print(x + y)
+
+    soma(1, 2) -> argumentos não nomeados/posicionais
+    soma(y=2, x=1) -> argumentos nomeados (keyword argument)
+"""
